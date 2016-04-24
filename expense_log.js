8eced0a040bcf26569c4b8b283248c9412dcd8e7
Pratyush Nalam <pratyushnalam@outlook.com>
2016-04-24 15:57:08 -0400

    Committing @svpraneethnaidu's initial work

---
+++ expense_log.js
@@ -0,0 +1,10 @@
+var expense_log_raw =
+[
+	["09/11/2015", 1, "others", "Cash", 40.00],
+	["09/11/2015", 0, "others", "Airport Taxi", 17.50],
+	["09/14/2015", 1, "loan", "International DD", 12000.00],
+	["09/15/2015", 0, "rent", "September", 378.80],
+	["09/15/2015", 0, "tuition", "FA 15 Fee", 10512.21],
+	["09/15/2015", 1, "others", "Check Deposit", 2500.00],
+	["09/21/2015", 1, "others", "Pratyush", 40.00],
+]
